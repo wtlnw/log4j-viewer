7a08fa72b49784ed75b65cf980d013c92643e549
wtlnw.github <wtlnw.github@gmail.com>
2025-11-30 22:38:12 +0100

    Implemented support for XML serialized log events.

---
+++ plugins/log4j.viewer.core.xml.test/src/log4j/viewer/core/xml/test/TestXmlLogEventSupplierFactory.java
@@ -0,0 +1,127 @@
+package log4j.viewer.core.xml.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.wtlnw.eclipse.log4j.viewer.core.api.LogEventSupplierFactory;
+import org.wtlnw.eclipse.log4j.viewer.core.impl.LogEventServer;
+
+import log4j.viewer.core.xml.impl.XmlLogEventSupplierFactory;
+
+/**
+ * Tests for {@link TestXmlLogEventSupplierFactory}.
+ */
+public class TestXmlLogEventSupplierFactory {
+
+	private static final String CONFIG_COMPLETE = """
+			<Configuration xmlns="https://logging.apache.org/xml/ns"
+			               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+			               xsi:schemaLocation="
+			                   https://logging.apache.org/xml/ns
+			                   https://logging.apache.org/xml/ns/log4j-config-2.xsd">
+			  <Appenders>
+			    <Socket name="SOCKET_APPENDER" host="localhost" port="4445">
+			      <XmlLayout complete="true"/>
+			    </Socket>
+			  </Appenders>
+			  <Loggers>
+			    <Root level="INFO">
+			      <AppenderRef ref="SOCKET_APPENDER"/>
+			    </Root>
+			  </Loggers>
+			</Configuration>
+			""";
+
+	private static final String CONFIG_FRAGMENTS = """
+			<Configuration xmlns="https://logging.apache.org/xml/ns"
+			               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+			               xsi:schemaLocation="
+			                   https://logging.apache.org/xml/ns
+			                   https://logging.apache.org/xml/ns/log4j-config-2.xsd">
+			  <Appenders>
+			    <Socket name="SOCKET_APPENDER" host="localhost" port="4445">
+			      <XmlLayout complete="false"/>
+			    </Socket>
+			  </Appenders>
+			  <Loggers>
+			    <Root level="INFO">
+			      <AppenderRef ref="SOCKET_APPENDER"/>
+			    </Root>
+			  </Loggers>
+			</Configuration>
+			""";
+
+	@Test
+	void testComplete() throws IOException, InterruptedException {
+		runWithConfiguration(config(CONFIG_COMPLETE));
+	}
+
+	@Test
+	void testFragments() throws IOException, InterruptedException {
+		runWithConfiguration(config(CONFIG_FRAGMENTS));
+	}
+	
+	@SuppressWarnings("deprecation")
+	private void runWithConfiguration(final Configuration config) throws IOException, InterruptedException {
+		// use a semaphore to block until the handler thread fails
+		// which is a signal for us that either an error occurred or
+		// end of stream was reached.
+		final Semaphore sema = new Semaphore(0);
+		
+		final List<LogEvent> events = new ArrayList<>();
+		final List<LogEventSupplierFactory> factories = List.of(new XmlLogEventSupplierFactory());
+		final List<Throwable> errors = new ArrayList<>();
+		final LogEventServer server = new LogEventServer(factories, events::add);
+
+		server.start();
+		server.addErrorListener((msg, ex) -> {
+			if (ex instanceof EOFException) {
+				// ignore EOF
+			} else {
+				errors.add(ex);
+			}
+			sema.release();
+		});
+
+		try (final LoggerContext context = Configurator.initialize(config)) {
+			final Logger logger = context.getLogger(TestXmlLogEventSupplierFactory.class.getSimpleName());
+			logger.info("Information message");
+			logger.warn("Warning message");
+			logger.error("Error message", new RuntimeException());
+		}
+
+		sema.acquire();
+		server.stop();
+		
+		if (!errors.isEmpty()) {
+			errors.getFirst().printStackTrace();
+			Assertions.fail();
+		}
+		Assertions.assertEquals(3, events.size());
+		Assertions.assertEquals("Information message", events.get(0).getMessage().getFormattedMessage());
+		Assertions.assertEquals("Warning message", events.get(1).getMessage().getFormattedMessage());
+		Assertions.assertEquals("Error message", events.get(2).getMessage().getFormattedMessage());
+		
+		Assertions.assertNotNull(events.get(2).getThrownProxy());
+	}
+
+	private Configuration config(final String config) throws IOException {
+		try (final InputStream input = new ByteArrayInputStream(config.getBytes())) {
+			return ConfigurationFactory.getInstance().getConfiguration(null, new ConfigurationSource(input));
+		}
+	}
+}
---
+++ plugins/log4j.viewer.core.xml/src/log4j/viewer/core/xml/impl/XmlLogEventSupplierFactory.java
@@ -0,0 +1,103 @@
+package log4j.viewer.core.xml.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Vector;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.core.jackson.Log4jXmlObjectMapper;
+import org.wtlnw.eclipse.log4j.viewer.core.api.LogEventSupplierFactory;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MappingIterator;
+
+/**
+ * {@link LogEventSupplierFactory} implementation for XML based {@link LogEvent}s.
+ */
+public class XmlLogEventSupplierFactory implements LogEventSupplierFactory {
+
+	@Override
+	public LogEventSupplier get(final InputStream stream) throws IOException {
+		if (!stream.markSupported()) {
+			throw new IllegalArgumentException();
+		}
+
+		stream.mark(16);
+		try {
+			final byte[] bytes = stream.readNBytes(16);
+			final String string = new String(bytes).trim();
+			final boolean complete = string.startsWith("<?xml");
+			final boolean fragment = string.startsWith("<Event");
+			if (complete || fragment) {
+				// make sure to reset the stream prior to supplier initialization
+				stream.reset();
+				return new XmlLogEventSupplier(stream, complete);
+			}
+		} catch (final Exception ex) {
+			// failed to read from the stream -> reset the stream
+			stream.reset();
+		}
+
+		// either we do not support the stream or we failed reading from it
+		return null;
+	}
+
+	/**
+	 * A {@link LogEventSupplier} implementation using {@link Log4jXmlObjectMapper}.
+	 */
+	private static class XmlLogEventSupplier implements LogEventSupplier {
+
+		/**
+		 * The {@link MappingIterator} for {@link LogEvent} de-serialization.
+		 */
+		private final MappingIterator<LogEvent> _reader;
+
+		/**
+		 * Create an {@link XmlLogEventSupplier} to read events from the given stream.
+		 * 
+		 * @param stream   the {@link InputStream} to read the {@link LogEvent}s from
+		 * @param complete {@code true} if the contents of the given stream is a
+		 *                 well-formed XML document or {@code false} for XML fragments
+		 *                 only
+		 * @throws IOException if an error occurred during initialization
+		 */
+		public XmlLogEventSupplier(final InputStream stream, final boolean complete) throws IOException {
+			final InputStream input;
+			if (!complete) {
+				final Vector<InputStream> streams = new Vector<>();
+				streams.add(new ByteArrayInputStream("<Events xmlns=\"https://logging.apache.org/log4j/2.0/events\">".getBytes()));
+				streams.add(stream);
+				streams.add(new ByteArrayInputStream("</Events>".getBytes()));
+				
+				input = new SequenceInputStream(streams.elements());
+			} else {
+				input = stream;
+			}
+			
+			_reader = new Log4jXmlObjectMapper()
+					.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+					.readerFor(Log4jLogEvent.class)
+					.readValues(input);
+		}
+
+		@Override
+		public LogEvent get() throws IOException {
+			try {
+				if (_reader.hasNext()) {
+					return _reader.next();
+				}
+			} catch (final RuntimeException ex) {
+				// make sure to wrap RuntimeExceptions in an IOException
+				// to terminate the handler thread correctly
+				throw new IOException(ex);
+			}
+			
+			// when we get here, we're done reading from the stream
+			throw new EOFException();
+		}
+	}
+}
